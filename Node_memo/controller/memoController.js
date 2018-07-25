d4a08360559c29d90171dd91ae775072148806bc
callor <callor@callor.com>
2018-07-25 18:08:21 +0900

    2018-07-25

--- Node_memo/controller/memoController.js
+++ Node_memo/controller/memoController.js
@@ -4,7 +4,9 @@ module.exports = (app,models)=>{
 	var memberVO = models.memberVO
 	
 	app.get('/',(req,res)=>{
-		res.render('index')
+		memoVO.find((err,data)=>{
+			res.render('index',{memoList:data})	
+		})
 	})
 	
 	app.get('/memo',(req,res)=>{
@@ -21,6 +23,8 @@ module.exports = (app,models)=>{
 	app.post('/memo',(req,res)=>{
 		if(!req.session.login) res.redirect('/login')
 		
+		// 만약에 메모 본문에 enter 표시가 있으면
+		req.body.memo = req.body.memo.replace(/(?:\r\n|\r|\n)/g,'<br/>')
 		memoVO.findById(req.body.id,
 				(err,data)=>{
 					if(data){
@@ -32,8 +36,8 @@ module.exports = (app,models)=>{
 					} else {
 						let vo = new memoVO(req.body)
 						vo.save((err,data)=>{
-							// res.redirect('/')
-							res.json(data)
+							 res.redirect('/')
+// res.json(data)
 						})
 					}
 				}
@@ -47,8 +51,34 @@ module.exports = (app,models)=>{
 		.exec((err,data)=>{
 			res.json(data)
 		})
-		
 	})
 	
-	
+	app.get('/edit/:id',(req,res)=>{
+		 
+		if(!req.session.login) {
+			res.redirect('/login')
+		} else {
+			// 글을 읽어와서
+			memoVO.findById(req.params.id)
+			.populate('writer')
+			// .where('userid').eq(req.session.login.userid)
+			.exec((err,data)=>{
+				// res.json({data:data,se:req.session})
+				if(data) {
+					if(req.session.login.userid == data.writer.userid) {
+						res.render('memo_form',{memo:data})	
+					} else {
+					res.render('clips/error',
+						 {error_msg:'본인이 작성한 글만 편집 가능'})
+						
+					}
+				} else {
+					res.redirect('/')
+				}
+			})
+		}
+	})
 }
+
+
+
